eef4f64b31331442759972ae8051e6edc3fac02a
wangweiwei <964788039@qq.com>
2018-05-03 09:31:29 +0800

    GetMapping

--- helloworld/src/main/java/me/www/helloworld/controller/HelloWorldController.java
+++ helloworld/src/main/java/me/www/helloworld/controller/HelloWorldController.java
@@ -1,6 +1,6 @@
 package me.www.helloworld.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,10 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String sayHello() {
         return "Hello,World!";
     }
-
 
 }
