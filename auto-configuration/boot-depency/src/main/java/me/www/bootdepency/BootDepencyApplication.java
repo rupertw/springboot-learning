b00c5016f125aa78cb33e266b110ae3b1a2188cd
www <rupertwong@163.com>
2018-05-08 22:54:26 +0800

    auto-configuration

---
+++ auto-configuration/boot-depency/src/main/java/me/www/bootdepency/BootDepencyApplication.java
@@ -0,0 +1,12 @@
+package me.www.bootdepency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDepencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootDepencyApplication.class, args);
+    }
+}
---
+++ auto-configuration/lrucache-api/src/main/java/LRUCache.java
@@ -0,0 +1,20 @@
+import java.util.Set;
+
+/**
+ * @author: www
+ * @date: 2018/5/8 22:41
+ * @description: TODO
+ */
+public interface LRUCache<K, V> {
+
+    void put(K key, V value);
+
+    V get(K key);
+
+    void remove(K key);
+
+    void clear();
+
+    Set<K> keySet();
+
+}
