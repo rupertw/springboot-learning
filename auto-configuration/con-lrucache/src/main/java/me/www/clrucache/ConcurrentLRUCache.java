8e070f735777ce889ad2cee82c170ed18a9d7455
www <rupertwong@163.com>
2018-05-09 23:57:21 +0800

    ConcurrentLRUCache

---
+++ auto-configuration/con-lrucache/src/main/java/me/www/clrucache/ConcurrentLRUCache.java
@@ -0,0 +1,33 @@
+package me.www.clrucache;
+
+import me.www.lrucache.LRUCache;
+
+import java.util.Set;
+
+/**
+ * @author: www
+ * @date: 2018/5/9 23:12
+ * @description: TODO
+ */
+public class ConcurrentLRUCache<K, V> implements LRUCache<K, V> {
+
+    public void put(Object key, Object value) {
+
+    }
+
+    public Object get(Object key) {
+        return null;
+    }
+
+    public void remove(Object key) {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public Set keySet() {
+        return null;
+    }
+}
