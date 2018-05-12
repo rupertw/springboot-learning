d1ccad118ee773f62dd95059131cd24a6b3abd0e
www <rupertwong@163.com>
2018-05-12 18:50:59 +0800

    lrucache-spring-boot-starter demo

--- auto-configuration/boot-depency/src/main/java/me/www/bootdepency/BootDepencyApplication.java
+++ auto-configuration/boot-depency/src/main/java/me/www/bootdepency/BootDepencyApplication.java
@@ -1,12 +1,22 @@
 package me.www.bootdepency;
 
+import me.www.lrucache.LRUCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class BootDepencyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootDepencyApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(BootDepencyApplication.class, args);
+
+        Map<String, LRUCache> beansMap = applicationContext.getBeansOfType(LRUCache.class);
+        for (Map.Entry<String, LRUCache> entry : beansMap.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
     }
 }
---
+++ auto-configuration/boot-depency/src/main/java/me/www/bootdepency/DemoController.java
@@ -0,0 +1,32 @@
+package me.www.bootdepency;
+
+import me.www.lrucache.LRUCache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: www
+ * @date: 2018/5/12 18:29
+ * @description: TODO
+ */
+@RestController
+public class DemoController {
+
+    @Autowired
+    private LRUCache lruCache;
+
+    @GetMapping("demo")
+    public String demo() {
+        lruCache.put("k1", "v1");
+        lruCache.put("k2", "v2");
+        lruCache.put("k3", "v3");
+        lruCache.put("k4", "v4");
+        lruCache.get("k1");
+        lruCache.put("k5", "v5");
+        System.out.println(lruCache); // NonConcurrentLRUCache{k3=v3, k4=v4, k1=v1, k5=v5}
+
+        return "demo";
+    }
+
+}
