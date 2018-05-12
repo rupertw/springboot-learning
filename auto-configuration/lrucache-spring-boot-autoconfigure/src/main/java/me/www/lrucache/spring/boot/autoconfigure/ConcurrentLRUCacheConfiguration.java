ee9808d8ff0c43c6106774de2afbc5c2f149c5ee
www <rupertwong@163.com>
2018-05-12 23:12:28 +0800

    lrucache-spring-boot-autoconfigure add log

--- auto-configuration/lrucache-spring-boot-autoconfigure/src/main/java/me/www/lrucache/spring/boot/autoconfigure/ConcurrentLRUCacheConfiguration.java
+++ auto-configuration/lrucache-spring-boot-autoconfigure/src/main/java/me/www/lrucache/spring/boot/autoconfigure/ConcurrentLRUCacheConfiguration.java
@@ -2,6 +2,8 @@ package me.www.lrucache.spring.boot.autoconfigure;
 
 import me.www.clrucache.ConcurrentLRUCache;
 import me.www.lrucache.LRUCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -19,15 +21,17 @@ import org.springframework.context.annotation.Configuration;
 @Conditional({LRUCacheCondition.class})
 class ConcurrentLRUCacheConfiguration {
 
+    private static final Logger logger = LoggerFactory.getLogger(ConcurrentLRUCacheConfiguration.class);
+
     private final LRUCacheProperties lruCacheProperties;
 
     public ConcurrentLRUCacheConfiguration(LRUCacheProperties lruCacheProperties) {
         this.lruCacheProperties = lruCacheProperties;
     }
 
-
     @Bean
     public ConcurrentLRUCache lruCache() {
+        logger.info("LRUCache: Bean of Type ConcurrentLRUCache is initialized");
         return new ConcurrentLRUCache(lruCacheProperties.getCacheSize());
     }
 
--- auto-configuration/lrucache-spring-boot-autoconfigure/src/main/java/me/www/lrucache/spring/boot/autoconfigure/NonConcurrentLRUCacheConfiguration.java
+++ auto-configuration/lrucache-spring-boot-autoconfigure/src/main/java/me/www/lrucache/spring/boot/autoconfigure/NonConcurrentLRUCacheConfiguration.java
@@ -2,6 +2,8 @@ package me.www.lrucache.spring.boot.autoconfigure;
 
 import me.www.lrucache.LRUCache;
 import me.www.nclrucache.NonConcurrentLRUCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -19,15 +21,17 @@ import org.springframework.context.annotation.Configuration;
 @Conditional({LRUCacheCondition.class})
 class NonConcurrentLRUCacheConfiguration {
 
+    private static final Logger logger = LoggerFactory.getLogger(NonConcurrentLRUCacheConfiguration.class);
+
     private final LRUCacheProperties lruCacheProperties;
 
     NonConcurrentLRUCacheConfiguration(LRUCacheProperties lruCacheProperties) {
         this.lruCacheProperties = lruCacheProperties;
     }
 
-
     @Bean
     public NonConcurrentLRUCache lruCache() {
+        logger.info("LRUCache: Bean of Type NonConcurrentLRUCache is initialized");
         return new NonConcurrentLRUCache(lruCacheProperties.getCacheSize());
     }
 
