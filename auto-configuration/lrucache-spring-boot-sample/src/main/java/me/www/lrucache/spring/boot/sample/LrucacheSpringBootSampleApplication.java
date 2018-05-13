8a16156c01ff69f04fc29e8120a9c3784454bdb2
www <rupertwong@163.com>
2018-05-13 11:07:36 +0800

    manually exclude any auto-configuration that you never want to apply

--- auto-configuration/lrucache-spring-boot-sample/src/main/java/me/www/lrucache/spring/boot/sample/LrucacheSpringBootSampleApplication.java
+++ auto-configuration/lrucache-spring-boot-sample/src/main/java/me/www/lrucache/spring/boot/sample/LrucacheSpringBootSampleApplication.java
@@ -1,13 +1,19 @@
 package me.www.lrucache.spring.boot.sample;
 
 import me.www.lrucache.LRUCache;
+import me.www.lrucache.spring.boot.autoconfigure.LRUCacheAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import java.util.Map;
 
+/**
+ * @author www
+ */
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude = LRUCacheAutoConfiguration.class)
 public class LrucacheSpringBootSampleApplication {
 
     public static void main(String[] args) {
