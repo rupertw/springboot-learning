95344f4502b191db8f5bebb83b84694f052a7bde
www <rupertwong@163.com>
2018-05-17 22:45:49 +0800

    actuator init

---
+++ actuator/src/main/java/me/www/actuator/ActuatorApplication.java
@@ -0,0 +1,15 @@
+package me.www.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author www
+ */
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+}
